4aaf374d48c62388995fdebd2fb57671db3a99c9
Cycloctane <Cycloctane@outlook.com>
2024-05-05 16:16:36 +0800

    add error handling

--- engines/mikan.py
+++ engines/mikan.py
@@ -1,4 +1,4 @@
-# VERSION: 0.1
+# VERSION: 0.2
 # AUTHORS: Cycloctane (Cycloctane@octane.top)
 
 from typing import Union
@@ -7,39 +7,53 @@ from http.client import HTTPSConnection
 
 from novaprinter import prettyPrinter
 
-MIKAN_URL = "mikanime.tv"
 
 class mikan:
 
-    url: str = MIKAN_URL
     name: str = "MikanRSSEngine"
+    url: str = "mikanime.tv"
 
-    supported_categories = {
+    supported_categories: dict[str, str] = {
         'all': '',
         'anime': ''
     }
 
     @staticmethod
+    def __print_message(msg: str) -> None:
+        prettyPrinter({'engine_url': f'https://{mikan.url}/','seeds': -1, 'leech': -1, 'size': 0,
+                        'name': msg, 'link': 'no link','desc_link': f'https://{mikan.url}/'})
+
+    @staticmethod
     def __request(target: str) -> str:
-        conn = HTTPSConnection(mikan.url)
+        conn = HTTPSConnection(mikan.url, timeout=4)
         conn.request("GET", f"/RSS/Search?searchstr={target}", 
                     headers={'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"})
         res = conn.getresponse()
+        if res.status != 200: raise Exception(f"http status code {res.status}")
         return res.read().decode('utf-8')
 
     @staticmethod
     def __parse(text: str) -> None:
-        search_result = ElementTree.fromstring(text)
-        for item in search_result.find("channel").findall("item"):
-            row: dict[str, Union[str, int, None]] = {
-                'engine_url': 'https://mikanime.tv/','seeds': -1, 'leech': -1}
-            row['name'] = item.findtext("title")
-            row['link'] = item.find("enclosure").attrib.get('url', None)
-            row['size'] = item.find("enclosure").attrib.get('length', None)
-            row['desc_link'] = item.findtext('link')
-            prettyPrinter(row)
+        try:
+            search_result = ElementTree.fromstring(text)
+            if len(search_result.find("channel").findall("item")) == 0:
+                mikan.__print_message("no results found")
+                return
+            for item in search_result.find("channel").findall("item"):
+                row: dict[str, Union[str, int, None]] = {
+                    'engine_url': f'https://{mikan.url}/','seeds': -1, 'leech': -1}
+                row['name'] = item.findtext("title")
+                row['link'] = item.find("enclosure").attrib.get('url', None)
+                row['size'] = item.find("enclosure").attrib.get('length', None)
+                if None in row.values(): raise Exception("parse error")
+                row['desc_link'] = item.findtext('link')
+                prettyPrinter(row)
+        except (ElementTree.ParseError, AttributeError, KeyError): raise Exception("parse error")
 
 
     def search(self, what: str, cat: str = 'all') -> None:
-        response = mikan.__request(what)
-        mikan.__parse(response)
+        try:
+            response = mikan.__request(what)
+            mikan.__parse(response)
+        except Exception as e:
+            mikan.__print_message("error: "+str(e))
--- engines/nexus.py
+++ engines/nexus.py
@@ -1,4 +1,4 @@
-# VERSION: 0.1
+# VERSION: 0.2
 # AUTHORS: Cycloctane (Cycloctane@octane.top)
 
 from typing import Union
@@ -7,43 +7,62 @@ from http.client import HTTPSConnection
 
 from novaprinter import prettyPrinter
 
-PT_URL = ""
-PT_PASSKEY = ""
-PT_CATAGORIES = {
+# edit pt site url and passkey here.
+SITE_URL = ""
+PASSKEY = ""
+
+# check catagories in /getrss.php
+CATAGORIES = {
     'all': '', 'anime': '','games': '', 'movies': '',
     'music': '', 'software': '', 'tv': '',
 }
 
+
 class nexus:
 
     name: str = "NexusRSSEngine"
-    url: str = PT_URL
-    pt_passkey: str = PT_PASSKEY
+    url: str = SITE_URL
 
-    supported_categories: dict[str, str] = PT_CATAGORIES
+    supported_categories: dict[str, str] = CATAGORIES
+
+    @staticmethod
+    def __print_message(msg: str) -> None:
+        prettyPrinter({'engine_url': f'https://{nexus.url}/','seeds': -1, 'leech': -1, 'size': 0,
+                        'name': msg, 'link': 'no link','desc_link': f'https://{nexus.url}/'})
 
     @staticmethod
     def __parse(text: str) -> None:
-        search_result = ElementTree.fromstring(text)
-        for item in search_result.find("channel").findall("item"):
-            row: dict[str, Union[str, int, None]] = {
-                'engine_url': f'https://{nexus.url}/','seeds': -1, 'leech': -1}
-            row['name'] = item.findtext("title")
-            row['link'] = item.find("enclosure").attrib.get('url', None)
-            row['size'] = item.find("enclosure").attrib.get('length', None)
-            row['desc_link'] = item.findtext('link')
-            prettyPrinter(row)
+        if text == "invalid passkey": raise Exception("invalid passkey")
+        try:
+            search_result = ElementTree.fromstring(text)
+            if len(search_result.find("channel").findall("item")) == 0:
+                nexus.__print_message("no results found")
+                return
+            for item in search_result.find("channel").findall("item"):
+                row: dict[str, Union[str, int, None]] = {
+                    'engine_url': f'https://{nexus.url}/','seeds': -1, 'leech': -1}
+                row['name'] = item.findtext("title")
+                row['link'] = item.find("enclosure").attrib.get('url', None)
+                row['size'] = item.find("enclosure").attrib.get('length', None)
+                if None in row.values(): raise Exception("parse error")
+                row['desc_link'] = item.findtext('link')
+                prettyPrinter(row)
+        except (ElementTree.ParseError, AttributeError, KeyError): raise Exception("parse error")
 
     @staticmethod
     def __request(target: str, cat: str) -> str:
-        conn = HTTPSConnection(nexus.url)
+        conn = HTTPSConnection(nexus.url, timeout=4)
         conn.request("GET",
-                     f"/torrentrss.php?search_mode=0&rows=50&passkey={nexus.pt_passkey}&search={target}{'&cat'+cat+'=1' if cat != '' else ''}&linktype=dl", 
+                     f"/torrentrss.php?search_mode=0&rows=50&passkey={PASSKEY}&search={target}{'&cat'+cat+'=1' if cat != '' else ''}&linktype=dl", 
                     headers={'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"})
         res = conn.getresponse()
+        if res.status != 200: raise Exception(f"http status code {res.status}")
         return res.read().decode('utf-8')
 
 
     def search(self, what: str, cat: str = 'all') -> None:
-        response = nexus.__request(what, nexus.supported_categories[cat])
-        nexus.__parse(response)
+        try:
+            response = nexus.__request(what, nexus.supported_categories[cat])
+            nexus.__parse(response)
+        except Exception as e:
+            nexus.__print_message("error: "+str(e))
