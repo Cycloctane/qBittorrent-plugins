b993014bb116085d9f4bfd11ee6eb093cead37a3
Cycloctane <Cycloctane@outlook.com>
2025-03-03 20:20:08 +0800

    format

--- engines/nexus.py
+++ engines/nexus.py
@@ -1,10 +1,10 @@
 # VERSION: 0.3
 # AUTHORS: Cycloctane (Cycloctane@octane.top)
 
-from typing import Union
-from xml.etree import ElementTree
 from http.client import HTTPSConnection
+from typing import Union
 from urllib.parse import urlparse
+from xml.etree import ElementTree
 
 from novaprinter import prettyPrinter
 
@@ -14,7 +14,7 @@ PASSKEY = ""
 
 # check catagories in /getrss.php
 CATAGORIES = {
-    'anime': '','games': '', 'movies': '',
+    'anime': '', 'games': '', 'movies': '',
     'music': '', 'software': '', 'tv': '',
 }
 
@@ -28,8 +28,8 @@ class nexus:
 
     @classmethod
     def __print_message(cls, msg: str) -> None:
-        prettyPrinter({'engine_url': cls.url,'seeds': -1, 'leech': -1, 'size': 0,
-                        'name': msg, 'link': 'no link','desc_link': cls.url})
+        prettyPrinter({'engine_url': cls.url, 'seeds': -1, 'leech': -1, 'size': 0,
+                       'name': msg, 'link': 'no link', 'desc_link': cls.url})
 
     @classmethod
     def __parse(cls, text: str) -> None:
@@ -41,25 +41,26 @@ class nexus:
                 return
             for item in search_result.find("channel").findall("item"):
                 row: dict[str, Union[str, int, None]] = {
-                    'engine_url': cls.url,'seeds': -1, 'leech': -1}
+                    'engine_url': cls.url, 'seeds': -1, 'leech': -1
+                }
                 row['name'] = item.findtext("title")
                 row['link'] = item.find("enclosure").attrib.get('url', None)
                 row['size'] = item.find("enclosure").attrib.get('length', None)
                 if None in row.values(): raise Exception("parse error")
                 row['desc_link'] = item.findtext('link')
                 prettyPrinter(row)
-        except (ElementTree.ParseError, AttributeError, KeyError): raise Exception("parse error")
+        except (ElementTree.ParseError, AttributeError, KeyError):
+            raise Exception("parse error")
 
     @classmethod
     def __request(cls, target: str, cat: str) -> str:
         conn = HTTPSConnection(urlparse(cls.url).hostname, urlparse(cls.url).port, timeout=4)
         conn.request("GET",
-                     f"/torrentrss.php?search_mode=0&rows=50&passkey={PASSKEY}&search={target}{'&cat'+cat+'=1' if cat != '' else ''}&linktype=dl", 
-                    headers={'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"})
+                     f"/torrentrss.php?search_mode=0&rows=50&passkey={PASSKEY}&search={target}{'&cat'+cat+'=1' if cat != '' else ''}&linktype=dl",
+                     headers={'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"})
         res = conn.getresponse()
         if res.status != 200: raise Exception(f"http status code {res.status}")
         return res.read().decode('utf-8')
-
 
     def search(self, what: str, cat: str = 'all') -> None:
         try:
--- engines/skyeysnow.py
+++ engines/skyeysnow.py
@@ -1,10 +1,10 @@
 # VERSION: 0.1
 # AUTHORS: Cycloctane (Cycloctane@octane.top)
 
-from typing import Union
-from xml.etree import ElementTree
 from http.client import HTTPSConnection
+from typing import Union
 from urllib.parse import urlparse
+from xml.etree import ElementTree
 
 from novaprinter import prettyPrinter
 
@@ -23,8 +23,8 @@ class skyeysnow:
 
     @classmethod
     def __print_message(cls, msg: str) -> None:
-        prettyPrinter({'engine_url': cls.url,'seeds': -1, 'leech': -1, 'size': 0,
-                        'name': msg, 'link': 'no link','desc_link': cls.url})
+        prettyPrinter({'engine_url': cls.url, 'seeds': -1, 'leech': -1, 'size': 0,
+                       'name': msg, 'link': 'no link', 'desc_link': cls.url})
 
     @classmethod
     def __parse(cls, text: str) -> None:
@@ -35,25 +35,26 @@ class skyeysnow:
                 return
             for item in search_result.find("channel").findall("item"):
                 row: dict[str, Union[str, int, None]] = {
-                    'engine_url': cls.url,'seeds': -1, 'leech': -1}
+                    'engine_url': cls.url, 'seeds': -1, 'leech': -1
+                }
                 row['name'] = item.findtext("title")
                 row['link'] = item.find("enclosure").attrib.get('url', None)
                 row['size'] = item.find("enclosure").attrib.get('length', None)
                 if None in row.values(): raise Exception("parse error")
                 row['desc_link'] = item.findtext('link')
                 prettyPrinter(row)
-        except (ElementTree.ParseError, AttributeError, KeyError): raise Exception("parse error")
+        except (ElementTree.ParseError, AttributeError, KeyError):
+            raise Exception("parse error")
 
     @classmethod
     def __request(cls, target: str) -> str:
         conn = HTTPSConnection(urlparse(cls.url).hostname, urlparse(cls.url).port, timeout=4)
         conn.request("GET",
-                    f"/ptrss.php?cat=1&search={target}&id={PASSKEY}",
-                    headers={'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"})
+                     f"/ptrss.php?cat=1&search={target}&id={PASSKEY}",
+                     headers={'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"})
         res = conn.getresponse()
         if res.status != 200: raise Exception(f"http status code {res.status}")
         return res.read().decode('utf-8')
-
 
     def search(self, what: str, cat: str = 'all') -> None:
         try:
